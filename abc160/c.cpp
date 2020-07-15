b129054c28243eda7ab7356eb65ee0bba80b1455
Yuta Matsuda <38342119+nena-undefined@users.noreply.github.com>
2020-07-15 16:59:32 +0900

    2020/7/15

---
+++ abc160/c.cpp
@@ -0,0 +1,67 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+#define REP(i,n) for(auto i=0;i<n;++i)
+#define REPR(i,n) for(auto i=n;i>=0;--i)
+#define REPI(itr,v) for(auto itr=v.begin();itr!=v.end();++itr)
+#define REPIR(itr,v) for(auto itr=v.rbegin();itr!=v.rend();++itr)
+#define FOR(i,a,b) for(auto i=a;i<b;++i)
+#define SORT(v,n) sort(v, v+n)
+#define SORTV(v) sort(v.begin(), v.end())
+#define ALL(v) v.begin(),v.end()
+#define llong long long
+#define ll long long
+#define INF 999999999
+#define MOD 1000000007
+#define pb push_back
+#define pf push_front
+#define MP make_pair
+#define SV(n,v) {int tmp;for(int i=0;i<n;++i){scanf("%d",&tmp);v.push_back(tmp);}}
+
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
+
+using namespace std;
+
+typedef pair<int,int> pii;
+
+int main(){
+
+  llong n, k;
+  cin >> k >> n;
+
+  vector<llong> a(n);
+  REP(i,n){
+    cin >> a[i];
+  }
+
+  llong ans = 0;
+  vector<llong> v;
+  REP(i,n){
+    v.pb(min(llabs(a[i] - a[(i+1)%n]), a[(i+1)%n] + (k - a[i])));
+    ans += v[i];
+  }
+
+  SORTV(v);
+
+  cout << ans - v[n-1] << endl;
+
+
+  return 0;
+
+}
+
+
+
